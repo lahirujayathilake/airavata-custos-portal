aabab9f3f480ecb3b4358d6bb1f2e27214facf1a
Dinuka De Silva <l.dinukadesilva@gmail.com>
2022-10-14 11:05:48 -0400

    Refactoring

--- src/lib/store/util/index.js
+++ src/lib/store/util/index.js
@@ -11,9 +11,6 @@ import ciLogon from "../modules/ci-logon.store";
 import {createLogger} from "vuex";
 
 export function createCustosStore(vuex = Vuex, vue = Vue) {
-    console.log("vue : ", vue);
-    console.log("vuex : ", vuex);
-
     vue.use(vuex);
 
     const debug = process.env.NODE_ENV !== 'production';
