d417a688645e5f2e0ee9799c60692dcadd1683e4
Marcus Christie <machristie@apache.org>
2022-11-07 14:48:14 -0500

    Add Gunicorn to Nginx/Docker setup

--- airavata_custos_portal/airavata_custos_portal/settings.py
+++ airavata_custos_portal/airavata_custos_portal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import json
 import os
 from pathlib import Path
 
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=3_8+_&j54+-&(j(s2v=p#7%#$)oihyl)rdmp8(^5#r3y2wbs)"
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-=3_8+_&j54+-&(j(s2v=p#7%#$)oihyl)rdmp8(^5#r3y2wbs)",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "true") == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = json.loads(os.environ.get("DJANGO_ALLOWED_HOSTS", '["localhost"]'))
 
 
 # Application definition
@@ -122,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+if not DEBUG:
+    STATIC_ROOT = os.environ["DJANGO_STATIC_ROOT"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 # Replace env vars in JavaScript files
-echo "Replacing env vars in JS"
-for file in /usr/share/nginx/html/js/app.*.js;
-do
-  echo "Processing $file ...";
+# echo "Replacing env vars in JS"
+# for file in /usr/share/nginx/html/js/app.*.js;
+# do
+#   echo "Processing $file ...";
 
-  # Use the existing JS file as template
-  if [ ! -f $file.tmpl.js ]; then
-    cp $file $file.tmpl.js
-  fi
+#   # Use the existing JS file as template
+#   if [ ! -f $file.tmpl.js ]; then
+#     cp $file $file.tmpl.js
+#   fi
 
-  envsubst '$VUE_APP_CLIENT_ID,$VUE_APP_CLIENT_SEC,$VUE_APP_REDIRECT_URI,$VUE_APP_CUSTOS_API_URL,$VUE_APP_SUPER_CLIENT_ID$VUE_APP_UNDER_MAINTENANCE' < $file.tmpl.js > $file
-done
+#   envsubst '$VUE_APP_CLIENT_ID,$VUE_APP_CLIENT_SEC,$VUE_APP_REDIRECT_URI,$VUE_APP_CUSTOS_API_URL,$VUE_APP_SUPER_CLIENT_ID$VUE_APP_UNDER_MAINTENANCE' < $file.tmpl.js > $file
+# done
+
+# TODO: run gunicorn in a separate docker container
+echo "Starting Gunicorn in background"
+gunicorn -b unix:/tmp/gunicorn.sock --daemon airavata_custos_portal.wsgi
 
 echo "Starting Nginx"
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
