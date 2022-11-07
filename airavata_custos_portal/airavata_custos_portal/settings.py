367adf5f01e0c90ab8e4531c833973bf52bfc762
Marcus Christie <machristie@apache.org>
2022-11-07 10:08:45 -0500

    Load environment variables in settings.py

--- airavata_custos_portal/airavata_custos_portal/settings.py
+++ airavata_custos_portal/airavata_custos_portal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,3 +142,16 @@ WEBPACK_LOADER = {
         "TIMEOUT": 60,
     },
 }
+
+VUE_APP_CLIENT_ID = os.environ.get("VUE_APP_CLIENT_ID", "")
+VUE_APP_CLIENT_SEC = os.environ.get("VUE_APP_CLIENT_SEC", "")
+VUE_APP_REDIRECT_URI = os.environ.get(
+    "VUE_APP_REDIRECT_URI", "http://localhost:8080/callback"
+)
+VUE_APP_CUSTOS_API_URL = os.environ.get(
+    "VUE_APP_CUSTOS_API_URL", "https://custos.scigap.org/apiserver"
+)
+VUE_APP_SUPER_CLIENT_ID = os.environ.get("VUE_APP_SUPER_CLIENT_ID", "")
+VUE_APP_UNDER_MAINTENANCE = (
+    os.environ.get("VUE_APP_UNDER_MAINTENANCE", "false") == "true"
+)
