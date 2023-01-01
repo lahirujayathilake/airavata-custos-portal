a349682def8ff842df44fc89bb08e19f06f494a0
Dinuka De Silva <l.dinukadesilva@gmail.com>
2023-01-01 11:35:53 -0500

    Adding custom headers to data to custos endpoints

--- airavata_custos_portal/airavata_custos_portal/apps/api/views.py
+++ airavata_custos_portal/airavata_custos_portal/apps/api/views.py
@@ -18,7 +18,7 @@ def get_config(request):
 
 VUE_APP_CUSTOS_API_URL = "https://prod.custos.usecustos.org/apiserver"
 ENDPOINTS = {
-    "IDENTITY": "/identity-management/v1.0.0",
+    "IDENTITY": "identity-management/v1.0.0",
     "USERS": "user-management/v1.0.0",
     "GROUPS": "group-management/v1.0.0",
     "TENANTS": "tenant-management/v1.0.0",
@@ -29,100 +29,124 @@ ENDPOINTS = {
 
 @api_view()
 def get_userinfo(request):
-    # return Response({
-    #     "access_token": request.session['access_token']
-    # })
-
-    payload = jwt.decode(jwt=request.session['access_token'], verify=False)
-
-    return Response(payload)
-
-
-
-@api_view()
-def get_auth(request):
-    response = requests.get(url=f"{VUE_APP_CUSTOS_API_URL}{ENDPOINTS['IDENTITY']}/.well-known/openid-configuration",
-                            params={'client_id': VUE_APP_CLIENT_ID})
-    response = response.json()
-    print("####### response ", response)
-    authorization_endpoint = response["authorization_endpoint"]
-    url = f"{authorization_endpoint}?response_type=code&client_id={VUE_APP_CLIENT_ID}&redirect_uri={VUE_APP_REDIRECT_URI}&scope=openid"
-
-    ciLogonInstitutionEntityId = False
-
-    if ciLogonInstitutionEntityId:
-        url = f"{url}&kc_idp_hint=oidc&idphint={ciLogonInstitutionEntityId}"
+    if 'access_token' not in request.session:
+        return HttpResponse('Unauthorized', status=401)
     else:
-        url = f"{url}&kc_idp_hint=oidc"
+        payload = jwt.decode(jwt=request.session['access_token'], verify=False)
+        return Response(payload)
 
-    return redirect(url)
+
+def get_client_auth_base64():
+    client_auth_base64 = f"{VUE_APP_CLIENT_ID}:{VUE_APP_CLIENT_SEC}"
+    client_auth_base64 = client_auth_base64.encode("utf-8")
+    client_auth_base64 = base64.b64encode(client_auth_base64).decode('utf-8')
+    client_auth_base64 = f"Bearer {client_auth_base64}"
+
+    return client_auth_base64;
 
 
 @api_view()
 def get_auth_callback(request):
     code = request.GET.get("code", None)
 
-    client_auth_base64 = f"{VUE_APP_CLIENT_ID}:{VUE_APP_CLIENT_SEC}"
-    client_auth_base64 = client_auth_base64.encode("utf-8")
-    client_auth_base64 = base64.b64encode(client_auth_base64).decode('utf-8')
-    client_auth_base64 = f"Bearer {client_auth_base64}"
+    client_auth_base64 = get_client_auth_base64()
 
-    response = requests.post(url=f"{VUE_APP_CUSTOS_API_URL}{ENDPOINTS['IDENTITY']}/token",
-                             json={'code': code, 'redirect_uri': VUE_APP_REDIRECT_URI,
-                                   'grant_type': 'authorization_code'},
-                             headers={
-                                 'Accept': '*/*',
-                                 'Content-Type': 'application/json',
-                                 'Authorization': client_auth_base64
+    response = requests.post(
+        url=f"{VUE_APP_CUSTOS_API_URL}/{ENDPOINTS['IDENTITY']}/token",
+        json={'code': code, 'redirect_uri': VUE_APP_REDIRECT_URI,
+              'grant_type': 'authorization_code'},
+        headers={
+            'Accept': '*/*',
+            'Content-Type': 'application/json',
+            'Authorization': client_auth_base64
 
-                             })
-    response = response.json()
+        }
+    )
 
-    # if request.session.test_cookie_worked():
-    #     request.session.delete_test_cookie()
-    #     return HttpResponse("You're logged in.")
-    # else:
-    #     return HttpResponse("Please enable cookies and try again.")
-
-    # request.session.set_test_cookie()
-
-    request.session.set_expiry(response["expires_in"])
-    request.session['access_token'] = response["access_token"]
-    request.session['refresh_token'] = response["refresh_token"]
-    request.session['id_token'] = response["id_token"]
+    set_token_response_session(request, response)
 
     return redirect("/")
-    # return render(request, "airavata_custos_portal_frontend/index.html")
-
-    # return Response({
-    #     "access_token": response["access_token"],
-    #     "refresh_token": response["refresh_token"],
-    #     "id_token": response["id_token"]
-    # })
 
 
-@api_view()
+def set_token_response_session(request, response):
+    response_json = response.json()
+    request.session.set_expiry(response_json["expires_in"])
+    request.session['access_token'] = response_json["access_token"]
+    request.session['refresh_token'] = response_json["refresh_token"]
+    request.session['id_token'] = response_json["id_token"]
+
+
+def remove_token_response_session(request):
+    del request.session['access_token']
+    del request.session['refresh_token']
+    del request.session['id_token']
+
+
+@api_view(["GET", "POST", "PUT", "DELETE"])
 def get_custos_api(request, endpoint_path=""):
-    if request.method == 'GET':
-        response = requests.get(url=f"{VUE_APP_CUSTOS_API_URL}/{endpoint_path}?{request.GET.urlencode()}",
-                                headers={
-                                    'Accept': '*/*',
-                                    'Content-Type': 'application/json',
-                                    'Authorization': f"Bearer {request.session['access_token']}"
-                                })
-    elif request.method == 'POST':
-        response = requests.post(url=f"{VUE_APP_CUSTOS_API_URL}/{endpoint_path}?{request.GET.urlencode()}",
-                                 json=request.POST,
-                                 headers={
-                                     'Accept': '*/*',
-                                     'Content-Type': 'application/json',
-                                     'Authorization': f"Bearer {request.session['access_token']}"
-                                 })
+    print(f"##### get_custos_api {request.method} {endpoint_path}")
 
-    return Response(response.json())
+    client_auth_base64 = get_client_auth_base64()
+    client_auth_cases_map = {
+        "token_password": endpoint_path == f"{ENDPOINTS['IDENTITY']}/token" and "grant_type" in request.data
+                          and request.data["grant_type"] == "password",
+        "token_refresh_token": endpoint_path == f"{ENDPOINTS['IDENTITY']}/token" and "grant_type" in request.data
+                               and request.data["grant_type"] == "refresh_token",
+        "token_authorization_code": endpoint_path == f"{ENDPOINTS['IDENTITY']}/token" and "grant_type" in request.data
+                                    and request.data["grant_type"] == "authorization_code",
+        "token_openid-configuration": endpoint_path == f"{ENDPOINTS['IDENTITY']}/.well-known/openid-configuration",
+        "logout": endpoint_path == f"{ENDPOINTS['IDENTITY']}/user/logout",
+        "tenant_create": endpoint_path == f"{ENDPOINTS['TENANTS']}/oauth2/tenant" and request.method == "POST"
+    }
 
-    # return Response({
-    #     "endpoint_path": endpoint_path,
-    #     "body": request.body,
-    #     "headers": request.headers
-    # })
+    authorization_header = ""
+    if True in client_auth_cases_map.values():
+        authorization_header = client_auth_base64
+    elif "access_token" in request.session:
+        authorization_header = f"Bearer {request.session['access_token']}"
+
+    headers = {
+        'Accept': '*/*',
+        'Content-Type': 'application/json',
+        'Authorization': authorization_header
+    }
+
+    print(f"##### get_custos_api {request.method} {endpoint_path} headers : ", headers)
+
+    url = f"{VUE_APP_CUSTOS_API_URL}/{endpoint_path}?{request.GET.urlencode()}"
+    data = request.data
+
+    if client_auth_cases_map["token_refresh_token"] or client_auth_cases_map["logout"]:
+        data['refresh_token'] = request.session['refresh_token']
+
+    response = requests.request(
+        method=request.method,
+        url=url,
+        json=data,
+        headers=headers
+    )
+    response_json = response.json()
+
+    print(f"##### get_custos_api {request.method} {endpoint_path} response_json : ", response_json)
+
+    if client_auth_cases_map["token_password"] or client_auth_cases_map["token_authorization_code"] or \
+            client_auth_cases_map["token_refresh_token"]:
+        set_token_response_session(request, response)
+        return Response(data={}, status=response.status_code)
+    elif client_auth_cases_map["token_openid-configuration"]:
+        authorization_endpoint = response_json["authorization_endpoint"]
+        url = f"{authorization_endpoint}?response_type=code&client_id={VUE_APP_CLIENT_ID}&redirect_uri={VUE_APP_REDIRECT_URI}&scope=openid"
+
+        ciLogonInstitutionEntityId = False  # TODO
+
+        if ciLogonInstitutionEntityId:
+            url = f"{url}&kc_idp_hint=oidc&idphint={ciLogonInstitutionEntityId}"
+        else:
+            url = f"{url}&kc_idp_hint=oidc"
+
+        return Response({"authorization_endpoint": url}, status=response.status_code)
+    elif client_auth_cases_map["logout"]:
+        remove_token_response_session(request)
+        return Response(data=response_json, status=response.status_code)
+    else:
+        return Response(data=response_json, status=response.status_code)
