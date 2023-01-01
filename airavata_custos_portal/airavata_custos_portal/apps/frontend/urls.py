1e880533b5e71850d2fe6f9a31103d41c150e579
Dinuka De Silva <l.dinukadesilva@gmail.com>
2023-01-01 11:36:15 -0500

    Adding all of the frontend routes

--- airavata_custos_portal/airavata_custos_portal/apps/frontend/urls.py
+++ airavata_custos_portal/airavata_custos_portal/apps/frontend/urls.py
@@ -5,5 +5,24 @@ from . import views
 
 app_name = "airavata_custos_portal_frontend"
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('tenants/default', views.home, name="home"),
+    path('tenants/<str:clientId>/child-tenants', views.home, name="home"),
+    path('tenants/<str:clientId>/child-tenants/new', views.home, name="home"),
+    path('tenants/<str:clientId>', views.home, name="home"),
+    path('tenants/<str:clientId>/profile', views.home, name="home"),
+    path('tenants/<str:clientId>/users', views.home, name="home"),
+    path('tenants/<str:clientId>/users/<str:username>', views.home, name="home"),
+    path('tenants/<str:clientId>/groups', views.home, name="home"),
+    path('tenants/<str:clientId>/groups/new', views.home, name="home"),
+    path('tenants/<str:clientId>/groups/<str:groupId>', views.home, name="home"),
+    path('tenants/<str:clientId>/permission-types/new', views.home, name="home"),
+    path('tenants/<str:clientId>/permission-types', views.home, name="home"),
+    path('tenants/<str:clientId>/roles/new', views.home, name="home"),
+    path('tenants/<str:clientId>/roles', views.home, name="home"),
+    path('tenants/<str:clientId>/entity-types/new', views.home, name="home"),
+    path('tenants/<str:clientId>/entity-types', views.home, name="home"),
+    path('tenants/<str:clientId>/entities/new', views.home, name="home"),
+    path('tenants/<str:clientId>/entities', views.home, name="home"),
+    path('tenants/<str:clientId>/entities/<str:entityId>', views.home, name="home"),
 ]
--- airavata_custos_portal/airavata_custos_portal/apps/frontend/views.py
+++ airavata_custos_portal/airavata_custos_portal/apps/frontend/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
+def home(request, **kwags):
     return render(request, "airavata_custos_portal_frontend/index.html")
 
